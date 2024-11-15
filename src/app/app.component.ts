1a4a11ee2245809390d2c65295114f68061ce640
PufAnimalsDev <baniukiewiczmateusz@gmail.com>
2024-11-15 17:15:51 +0100

    add practise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+export interface CardModel {
+  title: string;
+  image: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-root',
+  template: `
+    <div class="container">
+      <h1>Get all categories</h1>
+      <form >
+        @for (element of data$ | async; track element) {
+          <div class="form-check">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          [id]="element"
+          [value]="element"
+        />
+        <label class="form-check-label" [for]="element">
+          {{ element }}
+        </label>
+          </div>
+        }
+      </form>
+    </div>
+  `,
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [HttpClientModule, AsyncPipe],
 })
 export class AppComponent {
-  title = 'mentoring-program-starter-kit';
+  httpClient: HttpClient = inject(HttpClient);
+  data$: Observable<string[]> = this.httpClient.get<string[]>(
+    'https://fakestoreapi.com/products/categories'
+  );
 }
